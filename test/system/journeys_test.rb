4094cf2c6946284650d1303d1959d6190327e380
Theodor Vararu <theo@vararu.org>
2022-05-18 21:49:25 +0200

    Refactor test

--- test/system/journeys_test.rb
+++ test/system/journeys_test.rb
@@ -1,40 +1,67 @@
 require "application_system_test_case"
 
 class JourneysTest < ApplicationSystemTestCase
-  setup { @journey = journeys(:one) }
-
-  test "visiting the index" do
-    visit journeys_url
-    assert_selector "h1", text: "GOV.UK Mockup Kit"
+  setup do
+    Journey.create! title: "Foo"
+    Journey.create! title: "Bar"
   end
 
-  test "should create journey" do
+  test "end to end" do
+    given_i_am_on_the_journeys_page
+    then_i_see_two_journeys
+
+    when_i_click_create
+    then_i_see_the_title_form
+
+    when_i_click_continue
+    then_i_see_an_error
+
+    when_i_type_the_title
+    and_i_continue
+    then_i_see_the_journey_page
+
+    when_i_click_edit_title
+    then_i_see_the_title_form
+
+    when_i_click_continue
+    then_i_see_the_journey_page
+  end
+
+  def given_i_am_on_the_journeys_page
     visit journeys_url
+  end
+
+  def when_i_click_continue
+    click_on "Save and continue"
+  end
+  alias_method :and_i_continue, :when_i_click_continue
+
+  def when_i_click_create
     click_on "Create a journey"
-
-    fill_in "What is the name of your journey?", with: @journey.title
-    click_on "Save and continue"
-
-    assert_text "Journey was successfully created"
-    click_on "Back"
   end
 
-  test "should update Journey" do
-    visit journey_url(@journey)
-    click_on "Edit title", match: :first
-
-    fill_in "What is the name of your journey?", with: @journey.title
-    click_on "Save and continue"
-
-    assert_text "Journey was successfully updated"
-    click_on "Back"
+  def when_i_click_edit_title
+    click_on "Edit title"
   end
 
-  test "should destroy Journey" do
-    visit journey_url(@journey)
-    find(".govuk-details__summary-text", text: "Danger zone ⚠️").click
-    click_on "Delete this journey", match: :first
+  def when_i_type_the_title
+    fill_in "What is the name of your journey?", with: "Test journey"
+  end
 
-    assert_text "Journey was successfully destroyed"
+  def then_i_see_an_error
+    assert_selector "h2", text: "There is a problem"
+  end
+
+  def then_i_see_the_journey_page
+    assert_selector "h1", text: "Journey overview"
+  end
+
+  def then_i_see_the_title_form
+    assert_selector "h1", text: "What is the name of your journey?"
+  end
+
+  def then_i_see_two_journeys
+    assert_selector "dt", text: "Foo"
+    assert_selector "dt", text: "Bar"
   end
 end
