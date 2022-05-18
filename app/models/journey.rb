30acb2fe3281fba32ad349dd70bc5686f27af888
Theodor Vararu <theo@vararu.org>
2022-05-18 20:39:29 +0200

    Add solargraph-rails and annotate

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: journeys
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Journey < ApplicationRecord
   validates :title, presence: true
 end
--- test/models/journey_test.rb
+++ test/models/journey_test.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: journeys
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class JourneyTest < ActiveSupport::TestCase
