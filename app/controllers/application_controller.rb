ac4cded44a0a1b7244745ebf42d3fc3dc03d95d0
Theodor Vararu <theo@vararu.org>
2022-05-18 20:41:56 +0200

    Add message to basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   if Rails.env.production?
-    http_basic_authenticate_with name: "mockup", password: "kit"
+    http_basic_authenticate_with name: "mockup",
+                                 password: "kit",
+                                 message: "THIS IS NOT A REAL GOV.UK SERVICE"
   end
 
   default_form_builder GOVUKDesignSystemFormBuilder::FormBuilder
