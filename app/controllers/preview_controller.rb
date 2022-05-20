33fa7e154e0dbb806b59c36177c3cbda9303c1cb
Theodor Vararu <theo@vararu.org>
2022-05-20 23:45:35 +0200

    Add basic end to end preview functionality

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -1,5 +1,32 @@
 class PreviewController < ApplicationController
+  before_action :set_journey
+
   def show
+  end
+
+  def page
+    # TODO: Fix hacky code
+    @page = @journey.pages.find(params[:id])
+    current_index = @journey.pages.find_index(@page)
+    next_index = current_index + 1
+    next_page = @journey.pages.to_a.at(next_index)
+
+    @next_path =
+      (
+        if next_page.nil?
+          preview_confirmation_path
+        else
+          preview_page_path(id: next_page.id)
+        end
+      )
+  end
+
+  def confirmation
+  end
+
+  private
+
+  def set_journey
     @journey = Journey.find(params[:journey_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   end
 
   get "/preview/:journey_id", to: "preview#show", as: :preview
+  get "/preview/:journey_id/pages/:id", to: "preview#page", as: :preview_page
+  get "/preview/:journey_id/confirmation",
+      to: "preview#confirmation",
+      as: :preview_confirmation
 end
