7f0d6425c1750044d1264881f6c9b0a7d5b89755
Theodor Vararu <theo@vararu.org>
2022-05-22 10:51:06 +0200

    Do not pass in params directly into system call
    It is unsafe. Better to first try to find the journey, throw 404 if not
    found, and if found, use the id on the model to pass into the shell

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -1,12 +1,20 @@
 class ExportController < ApplicationController
+  before_action :set_journey
+
   # GET /journeys/1/export
   def show
   end
 
   # POST /journeys/1/export
   def create
-    system "rails export #{params[:journey_id]}"
+    system "rails export #{@journey.id}"
 
     redirect_to journey_export_path
+  end
+
+  private
+
+  def set_journey
+    @journey = Journey.find(params[:journey_id])
   end
 end
