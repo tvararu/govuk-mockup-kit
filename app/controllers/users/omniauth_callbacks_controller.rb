139a0bf69c90912b16f51ba9098625c1d64f1268
Theodor Vararu <theo@vararu.org>
2022-05-27 22:26:05 +0200

    Store session[:github_token]

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: :github
 
   def github
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    auth_hash = request.env["omniauth.auth"]
+    @user = User.from_omniauth(auth_hash)
+
+    session[:github_token] = auth_hash.credentials.token
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
@@ -11,10 +14,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         set_flash_message(:notice, :success, kind: "GitHub")
       end
     else
-      # Removing extra as it can overflow some session stores
-      session["devise.github_data"] = request.env["omniauth.auth"].except(
-        :extra
-      )
       redirect_to new_user_registration_url
     end
   end
